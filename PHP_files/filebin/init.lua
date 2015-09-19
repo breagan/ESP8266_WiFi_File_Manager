79305072c01c5f3308337e0fb754da38906c70f5
Bruce Reagan <github@airetraffic.com>
2015-09-19 11:06:12 -0400

    example file that can be sent to ESP

---
+++ PHP_files/filebin/init.lua
@@ -0,0 +1,13 @@
+wifi.setmode(wifi.STATION)
+wifi.sta.config("SSID","password")
+  print("System Info:  ")
+  print("IP: ")
+  print(wifi.sta.getip())
+    majorVer, minorVer, devVer, chipid, flashid, flashsize, flashmode, flashspeed = node.info();
+    print("NodeMCU "..majorVer.."."..minorVer.."."..devVer.."\nFlashsize: "..flashsize.."\nChipID: "..chipid)
+    print("FlashID: "..flashid.."\n".."Flashmode: "..flashmode.."\nHeap: "..node.heap())
+     -- get file system info
+    remaining, used, total=file.fsinfo()
+    print("\nFile system info:\nTotal : "..total.." Bytes\nUsed : "..used.." Bytes\nRemain: "..remaining.." Bytes")
+    print("\nReady")
+    dofile("servernode.lua")
