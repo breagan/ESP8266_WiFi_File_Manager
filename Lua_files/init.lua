c748b8502ef3974ce2cb1d9a9a16aebaaed0ec51
Bruce Reagan <github@airetraffic.com>
2015-09-19 11:01:08 -0400

    init.lua
    enter SSID and Password for local setup

---
+++ Lua_files/init.lua
@@ -0,0 +1,14 @@
+wifi.setmode(wifi.STATION)
+wifi.sta.config("SSID","password")   ---   SSID and Password for your LAN DHCP here
+  print("System Info:  ")
+  print("IP: ")
+  print(wifi.sta.getip())
+    majorVer, minorVer, devVer, chipid, flashid, flashsize, flashmode, flashspeed = node.info();
+    print("NodeMCU "..majorVer.."."..minorVer.."."..devVer.."\nFlashsize: "..flashsize.."\nChipID: "..chipid)
+    print("FlashID: "..flashid.."\n".."Flashmode: "..flashmode.."\nHeap: "..node.heap())
+     -- get file system info
+    remaining, used, total=file.fsinfo()
+    print("\nFile system info:\nTotal : "..total.." Bytes\nUsed : "..used.." Bytes\nRemain: "..remaining.." Bytes")
+    print("\nReady")
+    dofile("servernode.lua")    --  calls servernode.lua  
+    
