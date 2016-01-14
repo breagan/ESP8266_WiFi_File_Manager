0a420eee3262694e615e04361ea26cbbed75425e
breagan <github@airetraffic.com>
2016-01-14 14:38:38 -0500

    Update servernode.lua

--- Lua_files/servernode.lua
+++ Lua_files/servernode.lua
@@ -5,24 +5,17 @@ sv=net.createServer(net.TCP, 10)
     sv:listen(80,function(conn)
 		conn:on("receive", function(conn, pl) 
 		payload = pl;
-		conn:send("<html />")
-		conn:send("<body bgcolor='#E6E6E6'>")
-		conn:send("<B/>ESP8266 chipID: "..node.chipid())
-		conn:send("<br/>Running....!")
 		print(pl.."\n")
 ------------------------------------------------------------------------        
 		if string.sub(pl, 0, 11) == "**command**"  then
              dofile("wifi_tools.lua")
         end 
 -------------------------------------------------------------------------         
-        if string.find(pl, "status") then
 			dofile("getstatus.lua")
 			tmr.delay(250)
 			file.open("info.lua", "r")
-			conn:send("\n")
 			conn:send(file.read())
 			file.close("info.lua")
-         end
 		conn:close()
 		collectgarbage()
 		end)
